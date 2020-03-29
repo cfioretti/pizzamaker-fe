016ca8af2d5e2d716edc47bc8b80c50c7ea2324f
Lorenzo Benedetto <lorenzo@MacBook-Pro-di-Lorenzo.local>
2020-03-29 21:45:13 +0200

    WIP restructuring

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import './App.css';
-import AppBar from './Components/AppBar';
-import PanList from './Components/PanList';
-import Button from '@material-ui/core/Button';
+import Layout from './hoc/Layout/Layout';
+import PzManager from './Containers/PizzaManager/PizzaManager';
 
 function App() {
-  const [activity, setActivity] = React.useState();
-
-  const clickHandler = (event) => {
-    setActivity("addPan");
-  }
 
   return (
     <div className="App">
-      <header className="App-header">
-      <AppBar pageName="I love Pizza"/>
-      </header>
-      <body>
-        <PanList />
-        {activity === "addPan" ? <h1>Sto aggiungendo una padella</h1> : <h1>Sto per salvare il contenuto</h1>}
-        <Button size="medium" color="primary" variant="contained" onClick={clickHandler}>Calcola ingredienti</Button>
-      </body>
+      <Layout title="I love Pizza">
+        <PzManager/>
+      </Layout>
     </div>
   );
 }
