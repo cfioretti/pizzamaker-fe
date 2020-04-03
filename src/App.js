a15b4a39d0ff96c9af8765d9a87db6b1c16aef3e
Lorenzo Benedetto <lorenzo@MacBook-Pro-di-Lorenzo.local>
2020-04-03 12:41:21 +0200

    WIP v0.1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Layout from './hoc/Layout/Layout';
-import PzManager from './Containers/PizzaManager/PizzaManager';
+import PizzaManager from './Containers/PizzaManager/PizzaManager';
 
 function App() {
 
   return (
     <div className="App">
       <Layout title="I love Pizza">
-        <PzManager/>
+        <PizzaManager/>
       </Layout>
     </div>
   );
